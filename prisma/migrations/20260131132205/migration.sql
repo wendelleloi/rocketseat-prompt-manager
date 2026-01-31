15b8c82f7703ca2ba25af8f4d86b6ed17dd113f9
wendelleloi <wendelleloi2@gmail.com>
2026-01-31 10:31:35 -0300

    lesson 02

---
+++ prisma/migrations/20260131132205/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "prompts" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "prompts_pkey" PRIMARY KEY ("id")
+);
---
+++ src/lib/prisma.ts
@@ -0,0 +1,5 @@
+import { PrismaClient } from "@/generated/prisma/client";
+
+const prismaClient = new PrismaClient();
+
+export { prismaClient as prisma };
