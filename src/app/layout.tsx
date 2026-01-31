97daa5c75dbf9fbbc26c66b94dfe0bf3d8659aac
wendelleloi <wendelleloi2@gmail.com>
2026-01-31 15:24:18 -0300

    lesson 07

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.variable} antialiased bg-gray-900 text-white`}>
-        {children}
+        <section className="flex h-screen">
+          <main className="relative flex-1 overflow-auto min-w-0">
+            <div className="p-4 sm:p-6 md:p-8 max-w-full md:max-w-3xl mx-auto h-full">
+              {children}
+            </div>
+          </main>
+        </section>
       </body>
     </html>
   );
